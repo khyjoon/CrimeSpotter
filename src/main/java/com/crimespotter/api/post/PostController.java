b89c809dc82cb94d193ae7de21ffb1b8f2ad49db
Jason <s.jason@shaw.ca>
2020-04-05 20:17:06 -0700

    adding support for comments

--- src/main/java/com/crimespotter/api/post/PostController.java
+++ src/main/java/com/crimespotter/api/post/PostController.java
@@ -1,5 +1,6 @@
 package com.crimespotter.api.post;
 
+import com.crimespotter.api.post.model.comment.Comment;
 import com.crimespotter.api.post.model.post.Post;
 import com.crimespotter.api.post.model.post.PostCommunity;
 import com.crimespotter.api.post.model.post.PostEvent;
@@ -21,6 +22,14 @@ public class PostController {
                           @RequestParam("isActive") boolean isActive) {
         return postService.addPost(userId, eventId, isActive);
     }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/post/comment")
+    public List<Comment> addCommentToPost(@RequestParam("post_id") String postId,
+                                          @RequestParam("user_id") String userId,
+                                          @RequestParam("comment") String comment) {
+        return postService.addCommentOnPost(userId, postId, comment);
+    }
+
 
     @RequestMapping(method = RequestMethod.GET, value = "/post")
     public List<PostEvent> getPostInfo(@RequestParam(value="post_id", required=false) String postId,
--- src/main/java/com/crimespotter/api/post/model/comment/CommentMapper.java
+++ src/main/java/com/crimespotter/api/post/model/comment/CommentMapper.java
@@ -9,7 +9,7 @@ public class CommentMapper implements RowMapper<Comment> {
     @Override
     public Comment mapRow(ResultSet resultSet, int i) throws SQLException {
         Comment comment = new Comment();
-        comment.setPostId(resultSet.getString("comment_id"));
+        comment.setCommentId(resultSet.getString("comment_id"));
         comment.setUserId(resultSet.getString("user_id"));
         comment.setPostId(resultSet.getString("post_id"));
         comment.setContent(resultSet.getString("content"));
